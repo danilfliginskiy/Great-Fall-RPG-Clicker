ca41412e9cdb5caef6e84a7701bc825cb2eeb4bb
Retr0 <lara.korolleva@gmail.com>
2021-07-10 13:39:10 +0300

    Экран подтверждения

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -7,55 +7,59 @@ using UnityEngine.UI;
 public class Death : MonoBehaviour
 {
 
-    //Ссылки на другие скрипты
-    public WorkButton linkOnWorkButton;
-    public ChangeData linkOnChangeData;
-    public AddExperience linkOnAddExperience;
-
     public Text lvl;
     public Text levelProgress;
     public Text days;
     public Text hours;
     public Text money;
-
     public Slider progressBar;
     public GameObject DeathPanel;
-
     public Slider healthBar;
     public Slider hungerBar;
     public Slider happinessBar;
     public Slider depressionBar;
+    public GameObject RefinementPanel;
+    public GameObject AdvertisingPanel;
 
-
-    public void AceptButton() {
-
-        //Обнуление прогресса при смерти
-        progressBar.value = 0;
-        lvl.text = "1";
-        levelProgress.text = "0/20 XP";
-        linkOnAddExperience.level = 1;
-        linkOnAddExperience.index = 0;
-        progressBar.maxValue = linkOnAddExperience.amountOfProgress[0];
-        linkOnAddExperience.addedProgress = 1;
-
-        depressionBar.value = 0;
-        healthBar.value = 100;
-        hungerBar.value = 100;
-        happinessBar.value = 100;
-        money.text = "0";
-        linkOnWorkButton.amountOfMoney = 0;
-        //Добавить присовение первых элементов магазина
-        
-        days.text = "Дней: 0";
-        hours.text = "Часов: 0";
-        linkOnChangeData.amountOfDays = 0;
-        linkOnChangeData.amountOfHours = 0;
-
+    public void RefinementButton() //Вызывает панель уточнения
+    {
         DeathPanel.SetActive(false);
-
+        RefinementPanel.SetActive(true);
     }
 
-    public void PayOffButton() {
+    public void AdvertisingButton() //Вызывает панель уточнения
+    {
+        DeathPanel.SetActive(false);
+        AdvertisingPanel.SetActive(true);
+    }
+
+    public void NoButton() //Возвращает к панели смерти
+    {
+        RefinementPanel.SetActive(false);
+        AdvertisingPanel.SetActive(false);
+        DeathPanel.SetActive(true);
+    }
+
+
+    public void AceptButton() //Подтверждает выбор смерти
+    {
+        progressBar.value = 0f;
+        depressionBar.value = 0f;
+        healthBar.value = 100f;
+        hungerBar.value = 100f;
+        happinessBar.value = 100f;
+        lvl.text = "0";
+        levelProgress.text = "0/40 XP";
+        money.text = "0";
+        days.text = "Дней: 0";
+        hours.text = "Часов: 0";
+        RefinementPanel.SetActive(false);
+        DeathPanel.SetActive(false);
+
+
+    }
+    public void PayOffButton() //Запускает рекламу
+    {
 
     }
 }
